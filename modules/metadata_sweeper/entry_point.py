7fc8dffc1219b62a8b5fdcfc06fae12839cea156
Max00009 <mp7636681@gmail.com>
2025-12-07 23:52:50 +0530

    add main workflow logic

---
+++ modules/metadata_sweeper/entry_point.py
@@ -0,0 +1,25 @@
+'''this file is in testing mode right now.a lot of other files will be connected here cause it's like a junction.
+now it's in verbose and cli stage.when all done i will make it background daemon and silent.'''
+import sys
+import os
+from utils.file_detector import detect_file_type
+from cleaners.image_cleaner import image_cleaner
+from config import ERROR_FILE_NOT_FOUND
+
+def main():
+    filepath=sys.argv[1]
+    if not os.path.exists(filepath):
+        return ERROR_FILE_NOT_FOUND 
+    detected_file_type=detect_file_type(filepath)
+    if detected_file_type ==None:
+        print("detect file returned None.")
+    elif detected_file_type.startswith("image/"):
+        print("it detected jpeg and proceeding with next step")
+        result=image_cleaner(filepath)
+        if result=="SUCCESS":
+            print("yeah everything worked.check metadata now.")
+    else:
+        print("detect file returned something that doesn't starts with image.")
+
+if __name__=="__main__":
+    main()
