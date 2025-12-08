5346cd527a7b385ab7853c198b5ddf1cffde58be
Max00009 <mp7636681@gmail.com>
2025-12-08 23:23:02 +0530

    connect video cleaner wrapper

--- modules/metadata_sweeper/entry_point.py
+++ modules/metadata_sweeper/entry_point.py
@@ -4,6 +4,7 @@ import sys
 import os
 from utils.file_detector import detect_file_type
 from cleaners.image_cleaner import image_cleaner
+from cleaners.video_cleaner import video_cleaner
 from config import ERROR_FILE_NOT_FOUND
 
 def main():
@@ -14,12 +15,17 @@ def main():
     if detected_file_type ==None:
         print("detect file returned None.")
     elif detected_file_type.startswith("image/"):
-        print("it detected jpeg and proceeding with next step")
+        print("it detected image and proceeding with next step")
         result=image_cleaner(filepath)
         if result=="SUCCESS":
             print("yeah everything worked.check metadata now.")
+    elif detected_file_type.startswith("video/"):
+        print("it detected video and proceeding with next step")
+        result=video_cleaner(filepath)
+        if result=="SUCCESS":
+            print("yeah everything worked.check metadata now.")
     else:
-        print("detect file returned something that doesn't starts with image.")
+        print("detect file returned something that doesn't starts with image or video.")
 
 if __name__=="__main__":
     main()
