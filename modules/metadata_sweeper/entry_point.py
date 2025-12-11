f6b53d003bd21658529580ad1488222aff0bcdc7
Max00009 <mp7636681@gmail.com>
2025-12-11 22:03:12 +0530

    add pdf cleaner logic

--- modules/metadata_sweeper/entry_point.py
+++ modules/metadata_sweeper/entry_point.py
@@ -5,6 +5,7 @@ import os
 from utils.file_detector import detect_file_type
 from cleaners.image_cleaner import image_cleaner
 from cleaners.video_cleaner import video_cleaner
+from cleaners.pdf_cleaner import pdf_cleaner
 from config import ERROR_FILE_NOT_FOUND
 
 def main():
@@ -24,8 +25,12 @@ def main():
         result=video_cleaner(filepath)
         if result=="SUCCESS":
             print("yeah everything worked.check metadata now.")
+    elif detected_file_type=="application/pdf":#not starts with .exactly same.
+        result=pdf_cleaner(filepath)
+        if result=="SUCCESS":
+            print("yeah everything worked.check metadata now.")
     else:
-        print("detect file returned something that doesn't starts with image or video.")
+        print("detect file returned something that doesn't starts with image or video or pdf.")
 
 if __name__=="__main__":
     main()
