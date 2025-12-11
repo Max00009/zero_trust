f42335d37de2d6cad8d84a190f57e59e40051707
Max00009 <mp7636681@gmail.com>
2025-12-11 22:01:45 +0530

    add pdf cleaner main logic

---
+++ modules/metadata_sweeper/cleaners/pdf_cleaner.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <sys/stat.h>//for stat
+#include <cstdlib> //for system which is required to run shell command inside cpp
+extern "C"{
+const char* pdf_cleaner(const char* filepath){
+    //check if exiftool is present.i decided to do this step in the wrapper during import.so it will only happen once.
+    // int check=system("which exiftool > /dev/null 2>&1");//we only care about the return value.not the exact output text.that's why we are making the output disappear
+    // if(check!=0){
+    //     return "ERROR:EXIFTOOL_NOT_FOUND";
+    // }
+    //validates input
+    if(filepath==NULL ||filepath[0]=='\0'){
+        return "ERROR:FILE_NOT_FOUND";
+    }
+    //checks if file exists
+    struct stat file_info; //this is where file information of 'filepath' will be stored.needed for stat().stat will return 0 for success and -1 for fialure
+    if(stat(filepath,&file_info)!=0){
+        return "ERROR:FILE_NOT_FOUND";
+    }
+    //generate the command
+    std::string command = "exiftool -all= -overwrite_original \"" + std::string(filepath) + "\"";
+    //run command
+    int result=system(command.c_str());
+    //check result and return
+    if(result==0){
+        return "SUCCESS";
+    }else{
+        return "ERROR:CLEANING_FAILED";
+    }
+}
+}
