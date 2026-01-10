819d02046c2e043f984f134925fa4b7c01f262b2
Max00009 <mp7636681@gmail.com>
2026-01-10 15:32:15 +0530

    add some comments

--- modules/metadata_sweeper/config.py
+++ modules/metadata_sweeper/config.py
@@ -7,7 +7,7 @@ users can edit this file to change behaviour.
 2.However Image,Video,Pdf and audio will sometimes contain filesystem metadata(not file metadata) which is saved by os.
 exiftool can't remove those.some examples of filestructure metadata:
     File Modification Date/Time,<--we can change it using exiftool to set arbitary values.
-    File Access Date/Time,<--this gets changed as a side effect if and only if we change the FileModifyDate at last.good for us.
+    File Access Date/Time,<--SOMETIMES this gets changed as a side effect if and only if we change the FileModifyDate at last.good for us.
     File Inode Change Date/Time,
 3.There will also remain some internal dates/times after 'exiftool -all=' command as these live in
 atoms inside containers.for example-these are the internal MP4 dates:
@@ -20,23 +20,7 @@ atoms inside containers.for example-these are the internal MP4 dates:
 '''
 CHANGE_FILESYS_DATE=True #it will change the File Modification Date/Time
 CHANGE_INTERNAL_DATE=True #it will change the internal date/time
-REPLACE_ORIGINAL=True
-
-
-
-#supported formats
-SUPPORTED_FORMATS=[
-    #image
-    'image/jpeg',
-    'image/png',
-    'image/jpg',
-    'image/tiff',
-    'image/webp',
-    'image/heic',
-    #documents
-    #video
-    #audio
-]
+REPLACE_ORIGINAL=True #useless in case of id3v2
 
 #error messages
 ERROR_FILE_NOT_FOUND = "ERROR:FILE_NOT_FOUND"
@@ -48,3 +32,4 @@ ERROR_FILESYSTEM_METADATA_CLEANING_FAILED= "ERROR:FILESYSYSTEM_METADATA_CLEANING
 ERROR_INTERNAL_METADATA_CLEANING_FAILED= "ERROR:INTERNAL_METADATA_CLEANING_FAILED"
 ERROR_EXIFTOOL_NOT_FOUND= "ERROR:EXIFTOOL_NOT_FOUND"
 ERROR_METADATA_CLEANING_FAILED="ERROR:METADATA_CLEANING_FAILED"
+ERROR_ID3V2_FAILED="ERROR:ID3V2_FAILED"
