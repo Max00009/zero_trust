b41358de05afb06d5e4a5d7258abab135f45e4a9
Max00009 <mp7636681@gmail.com>
2026-02-08 23:37:01 +0530

    Add entry_point.py

---
+++ modules/traffic_shield/entry_point.py
@@ -0,0 +1,38 @@
+"""
+This is addon of mitmproxy.
+Usage: mitmweb -s requests_log.py --listen-host 0.0.0.0 --listen-port 8080
+"""
+from mitmproxy import http # type: ignore cause mitmproxy is not installed in my mac.it's in vm
+from datetime import datetime
+import os
+import config
+class Requestslog:
+    def __init__(self):
+        self.log_file="logs.txt"
+    #load is called once at startup.
+    def load(self,loader):#not using loader right now.may be later useful.
+        with open(self.log_file,'a') as f:
+            f.write(f"\n=== Started at {datetime.now()} ===\n")
+    #request called before forwarding to server
+    def request(self,flow:http.HTTPFlow):
+        timestamp=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        method=flow.request.method
+        url=flow.request.pretty_url
+        host = flow.request.host
+        scheme = flow.request.scheme
+        path = flow.request.path
+        
+        log_line=(
+            f"[{timestamp}]"
+            f"{method} {scheme}://{host}{path}"
+            f"{url}\n"
+            )
+        with open(self.log_file,'a') as f:
+            f.write(log_line)
+        print("logged")
+addons=[Requestslog()]#This tells mitmproxy to use this addon.
+'''mitmproxy will load requests_log.py.
+    create Requestslog() instance.
+    For every requests call appropriate methods accordingly'''
+config.MAX_THREAD_COUNT
+config.QUEUE_LENGTH
