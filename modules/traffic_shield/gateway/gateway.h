34414963d4e64a29563412aaebc446db6e2537ca
Max00009 <mp7636681@gmail.com>
2026-02-10 00:33:18 +0530

    Fix a typo and remove unnecessary function declaration

--- modules/traffic_shield/gateway/gateway.h
+++ modules/traffic_shield/gateway/gateway.h
@@ -1,6 +1,6 @@
 //this is header file which will contain all function signature.that way my cpp code will be cleaner.
-#ifndef GATEWAY.H
-#define GATEWAY.H
+#ifndef GATEWAY_H
+#define GATEWAY_H
 
 #ifdef __cplusplus
 extern "C"{
@@ -8,7 +8,6 @@ extern "C"{
 
 int gateway_init(int num_threads);//spawns all threads and then puts them in sleep untill a task is added in task_queue.
 int gateway_submit(const char* url);//submit a url into the queue.returns request_id if succeeds, -1 if queue full.
-void worker_function();//this one is the function every thread starts running.
 int gateway_get_result(int request_id);//returns 1 if result available,0 if still processing,-1 if error.
 void gateway_cleanup();//to close gateway
 #ifdef __cplusplus
