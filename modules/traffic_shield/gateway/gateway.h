e95784dbfa2e65a570a4f402b9cd0e44cfbd12b1
Max00009 <mp7636681@gmail.com>
2026-02-12 00:03:21 +0530

    Fix the gateway_init argument

--- modules/traffic_shield/gateway/gateway.h
+++ modules/traffic_shield/gateway/gateway.h
@@ -6,7 +6,7 @@
 extern "C"{
 #endif
 
-int gateway_init(int num_threads);//spawns all threads and then puts them in sleep untill a task is added in task_queue.
+int gateway_init(int num_threads,int queue_length);//spawns all threads and then puts them in sleep untill a task is added in task_queue.
 int gateway_submit(const char* url);//submit a url into the queue.returns request_id if succeeds, -1 if queue full.
 int gateway_get_result(int request_id);//returns 1 if result available,0 if still processing,-1 if error.
 void gateway_cleanup();//to close gateway
