45805aec5fc9c5e1c6ea87c4add0455dac12fbf3
Max00009 <mp7636681@gmail.com>
2026-02-15 01:40:04 +0530

    Add cache_init logic

--- modules/traffic_shield/entry_point.py
+++ modules/traffic_shield/entry_point.py
@@ -11,13 +11,16 @@ import config #to get values from config.py
 #fetch values from config.py
 thread_count=config.MAX_THREAD_COUNT
 queue_length=config.MAX_QUEUE_LENGTH
+max_size=config.MAX_CACHE_SIZE
+ttl=config.TIME_TO_LIVE
 
-#load gateway.so 
+#load all shared object file. 
 #i don't want to hardcode the path.I will use relative path.
 current_dir=os.path.dirname(os.path.abspath(__file__)) #current directory
-lib_path=os.path.join(current_dir,'gateway','gateway.so') #get the relative path.gateway is subdirectory and gateway.so is the shared object.
-gateway=ctypes.CDLL(lib_path) #now it will work from anywhere
-
+lib_path_gateway=os.path.join(current_dir,'gateway','gateway.so') #get the relative path.gateway is subdirectory and gateway.so is the shared object.
+gateway=ctypes.CDLL(lib_path_gateway) #now it will work from anywhere
+lib_path_cache=os.path.join(current_dir,'cache','cache.so')
+cache=ctypes.CDLL(lib_path_cache)
 
 #now we have to define function signature. i.e. what every function takes as arguments and what they returns.
 gateway.gateway_init.argtypes=[ctypes.c_int,ctypes.c_int]
@@ -26,6 +29,8 @@ gateway.gateway_submit.argtypes=[ctypes.c_char_p]
 gateway.gateway_submit.restype=ctypes.c_int
 #gateway.gateway_get_result.argtypes=ctypes.c_int
 #gateway.gateway_get_result.restype=ctypes.c_int
+cache.cache_init.argtypes=[ctypes.c_int64]
+cache.cache_init.restype=ctypes.c_int
 
 
 
@@ -34,12 +39,21 @@ class Urlsubmit:
         self.log_file="logs.txt"
     #load is called once at startup.
     def load(self,loader):#not using loader right now.may be later useful.
+        #First we initialize the gateway.
         print(f"Initializing gateway with {thread_count} threads...")
         result_of_gateway_init=gateway.gateway_init(thread_count,queue_length)
         if result_of_gateway_init==0:
             print("Gateway initiation successful.")
         else:
             print("Gateway initiation failed.")
+
+        #We will initialize our cache here.
+        result_of_cache_init=cache.cache_init(max_size,ttl)
+        if result_of_cache_init==0:
+            print("Cache initiation successful.")
+        else:
+            print("Cache initiation failed.")
+        
         with open(self.log_file,'a') as f:
             time=datetime.now()
             f.write(f"Starting url submission at {time}...\n")
