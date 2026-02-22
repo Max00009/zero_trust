ff4eff801a06e7a811065eb2e4ab26de45de0452
Max00009 <mp7636681@gmail.com>
2026-02-22 16:55:52 +0530

    Remove exiftool finding logic cause we are not using exiftool in this file

--- modules/metadata_sweeper/cleaners/audio_cleaner.cpp
+++ modules/metadata_sweeper/cleaners/audio_cleaner.cpp
@@ -4,14 +4,9 @@
 #include <cstdlib> //for system which is required to run shell command inside cpp
 extern "C"{
 const char* audio_cleaner(const char* filepath){
-    //check if exiftool is present.i decided to do this step in the wrapper during import.so it will only happen once.
-    // int check=system("which exiftool > /dev/null 2>&1");//we only care about the return value.not the exact output text.that's why we are making the output disappear
-    // if(check!=0){
-    //     return "ERROR:EXIFTOOL_NOT_FOUND";
-    // }
     //validates input
     if(filepath==NULL ||filepath[0]=='\0'){
-        return "ERROR:FILE_NOT_FOUND";
+        return "ERROR:INVALID_FILEPATH";
     }
     //checks if file exists
     struct stat file_info; //this is where file information of 'filepath' will be stored.needed for stat().stat will return 0 for success and -1 for fialure
