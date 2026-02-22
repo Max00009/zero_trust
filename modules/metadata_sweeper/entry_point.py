83bd38666f38a7bf76e3986c3237a7f1914ee4e9
Max00009 <mp7636681@gmail.com>
2026-02-22 16:58:48 +0530

    Update error handling and add system exit code

--- modules/metadata_sweeper/entry_point.py
+++ modules/metadata_sweeper/entry_point.py
@@ -7,12 +7,13 @@ from cleaners.image_cleaner import image_cleaner
 from cleaners.video_cleaner import video_cleaner
 from cleaners.pdf_cleaner import pdf_cleaner
 from cleaners.audio_cleaner import audio_cleaner
-from config import ERROR_FILE_NOT_FOUND
+
 
 def main():
     filepath=sys.argv[1]
     if not os.path.exists(filepath):
-        return ERROR_FILE_NOT_FOUND 
+        print("ERROR:FILE_NOT_FOUND")
+        sys.exit(1)
     detected_file_type=detect_file_type(filepath)
     if detected_file_type ==None:
         print("detect file returned None.")
@@ -22,25 +23,34 @@ def main():
         if result=="SUCCESS":
             print("yeah everything worked.check metadata now.")
         else:
-            return result
+            print(result)
+            sys.exit(1)
     elif detected_file_type.startswith("video/"):
         print("it detected video and proceeding with next step")
         result=video_cleaner(filepath)
         if result=="SUCCESS":
             print("yeah everything worked.check metadata now.")
         else:
-            return result
+            print(result)
+            sys.exit(1)
     elif detected_file_type.startswith("audio/"):#some recordings in newer model starts with video.video cleaner will handle that.
         print("it detected audio and proceeding with next step")
         result=audio_cleaner(filepath)
         if result=="SUCCESS":
             print("yeah everything worked.check metadata now.")
+        else:
+            print(result)
+            sys.exit(1)
     elif detected_file_type=="application/pdf":#not starts with .exactly same.
         result=pdf_cleaner(filepath)
         if result=="SUCCESS":
             print("yeah everything worked.check metadata now.")
+        else:
+            print(result)
+            sys.exit(1)
     else:
         print("detect file returned something that doesn't starts with image or video or pdf.")
+        sys.exit(1)
 
 if __name__=="__main__":
     main()
