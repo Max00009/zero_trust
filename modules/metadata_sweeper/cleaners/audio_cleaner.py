4a64270599eb5447cba4268f02db3544ae1affdc
Max00009 <mp7636681@gmail.com>
2026-02-22 16:56:41 +0530

    Remove all exiftool logic

--- modules/metadata_sweeper/cleaners/audio_cleaner.py
+++ modules/metadata_sweeper/cleaners/audio_cleaner.py
@@ -5,18 +5,6 @@ the wrapper will load the .so file,handle the ctype stuff once,convert python st
 import subprocess #to check exiftool
 import ctypes #to load the .so file
 import os #to make the relaitve file path
-#error messages
-from config import ERROR_EXIFTOOL_NOT_FOUND
-from config import ERROR_ID3V2_FAILED
-#now we will check if exiftool is installed ONNLY ONCE when module is loaded
-try:
-    subprocess.run(['which','exiftool'],
-                    stdout=subprocess.DEVNULL,#we are hiding text output
-                    stderr=subprocess.DEVNULL,#and error message.cause we only need to check if it's installed
-                    check=True)
-    _exiftool_available=True
-except subprocess.CalledProcessError:
-    _exiftool_available=False
 
 '''instead of hardcoding the file path of (file_detector.so) we have to build the relative file path
 in that way we can call our function from anywhere.__file__  will give the current directory'''
@@ -30,10 +18,6 @@ _lib.audio_cleaner.argtypes=[ctypes.c_char_p]
 _lib.audio_cleaner.restype=ctypes.c_char_p
 
 def audio_cleaner(filepath):
-    if _exiftool_available!=True:
-        return ERROR_EXIFTOOL_NOT_FOUND
     filepath_bytes=filepath.encode('utf-8')
     result=(_lib.audio_cleaner(filepath_bytes)).decode('utf-8')
-    if result=="ERROR:CLEANING_FAILED":
-        return ERROR_ID3V2_FAILED
     return result
