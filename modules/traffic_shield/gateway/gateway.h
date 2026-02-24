fd267c9e3b930a94d91ea5a4af933874bfffc415
Max00009 <mp7636681@gmail.com>
2026-02-24 22:57:38 +0530

    Add gateway_cleanup() signature

--- modules/traffic_shield/gateway/gateway.h
+++ modules/traffic_shield/gateway/gateway.h
@@ -8,8 +8,7 @@ extern "C"{
 
 int gateway_init(int num_threads,int queue_length);//spawns all threads and then puts them in sleep untill a task is added in task_queue.
 int gateway_submit(const char* url);//submit a url into the queue.returns request_id if succeeds, -1 if queue full.
-int gateway_get_result(int request_id);//returns 1 if result available,0 if still processing,-1 if error.
-void gateway_cleanup();//to close gateway
+void gateway_cleanup();//to close gateway.i will use it when gateway is opened for long but no requests are coming.it will also help to reset url_id.
 #ifdef __cplusplus
 }
 #endif
