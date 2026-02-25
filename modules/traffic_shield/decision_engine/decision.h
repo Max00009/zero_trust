5c892f56e3b392ba7cdf0018063dc3c2473108de
Max00009 <mp7636681@gmail.com>
2026-02-25 23:49:18 +0530

    update header file

--- modules/traffic_shield/decision_engine/decision.h
+++ modules/traffic_shield/decision_engine/decision.h
@@ -6,9 +6,7 @@
 extern "C"{
 #endif
 
-bool get_decision(size_t url_id); //we extract the decision from hashtable and erase that node from hashtable before sending to entry_point.py
-void decision_making(size_t url_id,size_t threat_intelligence_score); //will take data from gateway.cpp and then decide result and call update_hashtable to add the key,pair value.
-void update_hashtable(size_t url_id,bool decision);//take data from decision_making and add that to hashtable.
+bool decision_making(size_t threat_intelligence_score); //will take data from gateway.cpp and then decide result and call update_hashtable to add the key,pair value.
 #ifdef __cplusplus
 }
 #endif
