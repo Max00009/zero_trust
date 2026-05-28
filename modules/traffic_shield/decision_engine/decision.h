23a57b93bb7a5accf5068cbab6ecf616ff29d7be
Max00009 <mp7636681@gmail.com>
2026-05-29 03:05:42 +0530

    Remove extern C cause function is not being called from any pyhton code and change <stddef.h> to <cstddef>

--- modules/traffic_shield/decision_engine/decision.h
+++ modules/traffic_shield/decision_engine/decision.h
@@ -1,13 +1,10 @@
 //this is header file which will contain all function signature.that way my cpp code will be cleaner.
+//we don't need any extern C here cause this function is not being called from any pyhton code.
 #ifndef DECISION_H
 #define DECISION_H
-#include <stddef.h>
-#ifdef __cplusplus
-extern "C"{
-#endif
+#include <cstddef>
+
 
 bool decision_making(size_t threat_intelligence_score); //will take data from gateway.cpp and then decide result and call update_hashtable to add the key,pair value.
-#ifdef __cplusplus
-}
-#endif
+
 #endif
