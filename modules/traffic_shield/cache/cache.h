77b4c80ebab898303a4fabb9d12cf997e88605e9
Max00009 <mp7636681@gmail.com>
2026-05-29 02:56:27 +0530

    Change cache_fetch() function signature from const char* to const std::string& and move every function except cache_init() outside of extern C

--- modules/traffic_shield/cache/cache.h
+++ modules/traffic_shield/cache/cache.h
@@ -7,17 +7,15 @@ extern "C"{
 #endif
 //our functions declarations will be here.
 int cache_init(int max_size,int64_t ttl);   //returns 0 if successful.
-int cache_fetch(const char* url);   //returns threat_score if found and 0 if not found.
-int cache_insert();
-
-
-
-
 
 
 #ifdef __cplusplus
 }
 #endif
 
-
+//we put only cache_init inside extern C cause that's the only function our python code(entry_point.py) will call.
+//By placing other functions(which is not called from our python code) we can use c++ features(like std::string).
+//extern C block is used to indicate that the functions declared inside it should use C linkage. However, std::string is a C++ type, and C does not understand C++ types or the C++ standard library.
+int cache_fetch(const std::string& url);   //returns threat_score if found and 0 if not found.
+int cache_insert();
 #endif
