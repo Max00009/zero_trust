994fe284fb176f7dac7aa5890295ea3cc5c5467c
Max00009 <mp7636681@gmail.com>
2026-06-06 00:28:25 +0530

    Add a comment

--- modules/traffic_shield/decision_engine/decision.h
+++ modules/traffic_shield/decision_engine/decision.h
@@ -2,7 +2,7 @@
 //we don't need any extern C here cause this function is not being called from any pyhton code.
 #ifndef DECISION_H
 #define DECISION_H
-#include <cstddef>
+#include <cstddef> //for size_t
 
 
 bool decision_making(size_t threat_intelligence_score); //will take data from gateway.cpp and then decide result and call update_hashtable to add the key,pair value.
