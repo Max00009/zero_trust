3a5ffe48136072c984a54d7dcbed54c830f6d47a
Max00009 <mp7636681@gmail.com>
2026-06-12 00:20:33 +0530

    Add logic to check if parse was successful

--- modules/traffic_shield/analyzer/url_analyzer/main.cpp
+++ modules/traffic_shield/analyzer/url_analyzer/main.cpp
@@ -10,4 +10,12 @@ bool url_analyzer(std::string url){ //we receive a std::string.
     }
     //first we hwave to parse the url via the public parse() function.
     ParsedURL result=URLParser::parse(url);
+
+    //first check if parsing was successful
+    if (!result.parse_successfull){
+        return false; //malformed url
+    }
+
+    //now we will start analyzing
+    
 }
