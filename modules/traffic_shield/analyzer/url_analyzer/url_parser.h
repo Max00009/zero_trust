4ebf7ce13ef27887f7067bb152a460a1d80545d9
Max00009 <mp7636681@gmail.com>
2026-06-22 00:26:34 +0530

    add detect_host and credential_extractor function signatures

--- modules/traffic_shield/analyzer/url_analyzer/url_parser.h
+++ modules/traffic_shield/analyzer/url_analyzer/url_parser.h
@@ -16,12 +16,12 @@ struct ParsedURL{
     bool is_scheme_safe= false;
 
     //credentials
-    bool has_credentials= false;
+    bool has_credentials= false; // [username:password]@example.com  everything inside [] is credentials.
     std::string_view username;
     std::string_view password;
 
     //host
-    bool has_host=false; //if host is present then '://' is used after scheme name.if host not present then only ':' used.
+    bool has_host=true; //if host is present then '://' is used after scheme name.if host not present then only ':' used.http://[www.example.com]/path everything within [] is host.
     std::string_view full_host; //e.g.  "mail.google.com"  (no port)
     std::string_view subdomain; //e.g.  "mail"
     std::string registered_domain; //e.g.  "google.com"   (eTLD+1).We have to use std::string here cause we will have to lowercase it.
@@ -51,6 +51,7 @@ struct ParsedURL{
     bool double_encoding=false; //if % is also encoded(%25xx).
     bool is_punnycode=false;   //xn-- label in host.exploited in Homograph attacks.
     bool has_at_in_host=false;  //if @ present in host name.old trick.
+    bool blank_creds=false; //'@' is present but there is blank before that.suspicious.
 
     //status
     bool parse_successfull=true;   //false if url is fundamentally malformed.
@@ -68,13 +69,14 @@ public:
 private:
     //helper functions will live here
 
-    //NOTE:explain how we will modify the string even tho we are taking std::string_view.
+    //NOTE:explaination of how we will modify the string even tho we are taking std::string_view.
     //we can't modify the characters inside our string cause std::string_view is read only.
     //HOwever we can change where it points and how long it is.so we can just move the boundaries of the view, not touching the original string.
     static void trim_url(std::string_view& raw_url); //this function will remove all white-spaces from our url.We are passing by reference cause we don't want own copy.We want to trim the original copy.
     static void set_scheme(std::string_view& raw_url,ParsedURL& result); //this function will detect the first occurence of ':' and extract the scheme name.
-    static void is_host_present(std::string_view& raw_url,ParsedURL& result); //this function will check if '//' is present after ':'.
-
+    static void scheme_checker(ParsedURL& result); //this function will check if the scheme is safe and set 'is_scheme_safe' bool accordingly.
+    static void detect_host(std::string_view& raw_url,ParsedURL& result); //this function will check if '//' is present after ':'.
+    static void credential_extractor(std::string_view& raw_url,ParsedURL& result); //thsi function will check if any credentials present in our url.
 
 
 
