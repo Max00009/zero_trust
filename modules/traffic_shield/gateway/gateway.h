b8ca51d6308205edc189516983ad483d4ff62b7d
Max00009 <mp7636681@gmail.com>
2026-08-11 14:34:55 +0530

    change gateway_init() function signature

--- modules/traffic_shield/gateway/gateway.h
+++ modules/traffic_shield/gateway/gateway.h
@@ -6,7 +6,7 @@
 extern "C"{
 #endif
 
-int gateway_init(size_t number_of_threads,size_t length_of_queue); // spawns all threads and then puts them in sleep until a task is added in task_queue.
+int gateway_init(); // spawns all threads and then puts them in sleep until a task is added in task_queue.
 int gateway_submit(const char* url);//submit a url into the queue.returns the decision in 1/0 if succeeds, -1 if queue full.
 void gateway_cleanup();//to close gateway.i will use it when gateway is opened for long but no requests are coming.it will also help to reset url_id.
 #ifdef __cplusplus
